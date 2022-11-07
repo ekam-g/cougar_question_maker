d7b8b560a9cbbf56dd556fbc058346448d51292e
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-11-07 08:55:49 -0500

    better code

--- src/questions/mod.rs
+++ src/questions/mod.rs
@@ -1,3 +1,5 @@
+mod widgets;
+
 use std::str::Split;
 
 
@@ -13,21 +15,8 @@ pub fn write_questions_firestore_opened(what: Split<char>) {
     crate::writer::try_write(format!("const List<String> firestoreLocation = [{}];", what_info), true);
 }
 
+pub fn write_widgets () {
 
-
-
-struct widgets {}
-
-impl widgets {
-    pub fn make_open_questions(what: Vec<String>) -> String {
-        let mut return_data = "".to_owned();
-        for info in what {
-            // txt_writer::WriteData {}.add(info, LOCATION).expect("failed when writing, please fix and try again");
-            return_data = format!(r"openEndedQuestion(\n
-        what : {},\n
-        textCon : controler{}\n
-        )", info, info);
-        }
-        return_data
-    }
 }
+
+
