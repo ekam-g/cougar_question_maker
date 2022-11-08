dcf3949eecb44e2a1a464711f790e3ba5334fd0f
carghai <carghai74@gmail.com>
2022-11-08 14:00:59 -0500

    Works

--- output/questions.dart
+++ output/questions.dart
@@ -1,3 +1,4 @@
 const List<String> pitOpenEnded = ['answer2','answer1','answer2|Question2','answer1','Question',];
+const List<String> pitDropDownHeaders = ['Question2','Question',];
 const List<List<String>> pitDropDown = [['answer2','answer1',],['answer2','answer1',],];
 const List<String> scoutingOpenEnded = ['answer2','answer1','answer2|Question2','answer1','Question',];
--- src/questions/mod.rs
+++ src/questions/mod.rs
@@ -15,31 +15,31 @@ pub fn write_questions_firestore_opened(what: Split<char>, name : &str ,replace 
 }
 
 pub fn write_questions_firestore_drop_down<'a>(what: Split<'a, char>, name : &'a str) -> Result<() , &'a str>{
+    let first_data: String;
     let what_info: String = {
+        let mut header_vec: Vec<String> = vec![];
         let mut return_data: String = "".to_owned();
         for info in what {
-            dbg!(&info);
             let data = info.split(',');
-            dbg!(&data);
             let mut section = "".to_owned();
             if data.clone().count() < 2 {
                 return Err("Syntax Error, Please Make it Like this:  Question,answer1,answer2|Question2,answer1,answer2");
             }
             let mut first_done = false;
             for cool in data {
-                dbg!(&first_done);
                 if first_done {
                     section = format!("'{}',{}", cool, section);
-                    dbg!(&section);
                 }else {
+                    header_vec.push( cool.to_owned());
                     first_done = true;
                 }
             }
             return_data = format!("[{}],{}", section, return_data);
         }
+        first_data = header_vec.join(",");
         return_data
     };
-    dbg!(&what_info);
+    write_questions_firestore_opened(first_data.split(',') ,&format!("{}Headers", name),false);
     crate::writer::try_write(format!("const List<List<String>> {} = [{}];",name, what_info), false);
     Ok(())
 }
